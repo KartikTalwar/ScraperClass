a10bfe0f0e93d5d5d7d0caa9cdc51a9d1021c7fd
Kartik <talwar.kartik@gmail.com>
2011-04-02 00:02:53 -0400

    better file organization

--- examples.php
+++ examples.php
@@ -1,18 +1,29 @@
 <?php
 
 
+include "ScraperClass.php";
+
+
+
 
 $scraper = new Scraper;
 
 
-$link              = "http://www.bulletin.uwaterloo.ca/";
-$load             = $scraper->load($link);
-$cut               = $scraper->cut('<h3>When and where</h3>', '</div>', $load);
-$strip             = $scraper->strip($cut);
-$escape         = $scraper->escape($cut);
-$unescape     = $scraper->unescape($escape);
-$externalcss   = $scraper->externalcss($link);
-$externaljs     = $scraper->externaljs($link);
+$link = "http://www.bulletin.uwaterloo.ca/";
+
+$load = $scraper->load($link);
+
+$cut = $scraper->cut('<h3>When and where</h3>', '</div>', $load);
+
+$strip = $scraper->strip($cut);
+
+$escape = $scraper->escape($cut);
+
+$unescape = $scraper->unescape($escape);
+
+$externalcss = $scraper->externalcss($link);
+
+$externaljs = $scraper->externaljs($link);
 
 
 
